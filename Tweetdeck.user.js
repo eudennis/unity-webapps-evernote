dd524017d9f11096b7d71c2f93ddbecaf05e66ec
Dennis Altermann <dennisaltermann@gmail.com>
2014-02-21 21:44:12 -0300

    First commit

--- Tweetdeck.user.js
+++ Tweetdeck.user.js
@@ -1,14 +1,13 @@
 // ==UserScript==
 // @name Evernote-Unity-Integration
-// @include http://www.evernote.com/*
-// @include https://www.evernote.com/*
-// @version 1.01
+// @include https://tweetdeck.twitter.com/*
+// @version 1.0
 // @author Dennis Altermann
 // @require utils.js
 // ==/UserScript==
  
 window.Unity = external.getUnityObject(1.0);
  
-Unity.init({ name: "Evernote",
-            iconUrl: 'icon://unity-webapps-evernote',
+Unity.init({ name: "Tweetdeck",
+            iconUrl: 'icon://unity-webapps-tweetdeck',
             onInit: null });
